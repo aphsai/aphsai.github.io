47a90cfd773d7dd68a2d18e57d939598164cbd92
aphsai <saksham.a@hotmail.ca>
2017-10-13 22:57:27 -0400

    finished the circular version (less commits because I got it working locally by adding a choose file option

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 var audio = new Audio();
-audio.src = 'cocoa.mp3';
 audio.loop = true;
 audio.autoplay = true;
 audio.setAttribute("style","display:none;");
 var canvas, ctx, source, context, analyzer, fbc_array, bars, bar_x, bar_width, bar_height;
 
 window.onload = function init() {
+  document.getElementById("audio_file").onchange = function() {
+    var files = this.files;
+    var file = URL.createObjectURL(files[0]);
+    audio.src = file;
+    audio.play();
+  }
   document.getElementById('container').appendChild(audio);
   context = new AudioContext();
   analyzer = context.createAnalyser();
-  analyzer.smoothingTimeConstant = 0.3;
+  analyzer.smoothingTimeConstant = 0.8;
   canvas = document.getElementById('analyzer');
   canvas.setAttribute("width", document.body.getBoundingClientRect().width + "px");
   canvas.setAttribute("height", document.body.getBoundingClientRect().height + "px");
@@ -19,19 +24,25 @@ window.onload = function init() {
   source = context.createMediaElementSource(audio);
   source.connect(analyzer);
   analyzer.connect(context.destination);
+  ctx.translate(canvas.width/2, canvas.height/2);
   frameLooper();
+      // ctx.translate(canvas.width/2, canvas.height/2);
 }
 function frameLooper() {
   window.requestAnimationFrame(frameLooper);
   fbc_array = new Uint8Array(analyzer.frequencyBinCount);
   analyzer.getByteFrequencyData(fbc_array);
-  ctx.clearRect(0,0,canvas.width, canvas.height);
-  ctx.fillStyle = '#00CCFF';
+  ctx.clearRect(-canvas.width/2,-canvas.height/2,canvas.width, canvas.height);
   bars = 100;
   for (var i = 0; i < bars; i++) {
-    bar_width = canvas.width/bars;
-    bar_x = i * bar_width;
-    bar_height = -(fbc_array[i])/255 * canvas.height * 0.9;
-    ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+    bar_width = 8;
+    // bar_width = canvas.width/bars;
+    bar_height = -(fbc_array[i])/255 * canvas.height/2 * 0.9;
+    var grd = ctx.createLinearGradient(0,0,bar_width, bar_height);
+    grd.addColorStop(1, "#4d001f");
+    grd.addColorStop(0, "#333");
+    ctx.fillStyle = grd;
+    ctx.rotate(3.6*Math.PI/180)
+    ctx.fillRect(0, 0, bar_width, bar_height);
   }
 }
