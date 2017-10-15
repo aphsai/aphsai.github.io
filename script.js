74d9e22b2f4f0cb1aca22257137447a0a4b44b66
aphsai <saksham.a@hotmail.ca>
2017-10-14 20:57:39 -0400

    added triangles

--- script.js
+++ script.js
@@ -9,15 +9,32 @@ function createTriangle(x , y, check) {
   var posX = Math.floor(Math.random()*x);
   var posY = Math.floor(Math.random()*y);
   if (check == true) {
-    posY = Math.floor(Math.random()*y + y);
+    posY = Math.floor(Math.random()*(0.05) * y + y);
   }
-  triangle.setAttribute("style","width: 0; height: 0; border-left: " + 10 * size + "px solid transparent;  border-right: " + 10*size + "px solid transparent; border-bottom:" + size*17 + "px solid #111; position:absolute; top:" + posY + "px; left:" + posX  + "px;")
+  triangle.setAttribute("style","width: 0; height: 0; border-left: " + 10 * size + "px solid transparent;  border-right: " + 10*size + "px solid transparent; border-bottom:" + size*17 + "px solid #111; position:absolute;")
   triangle.setAttribute("data-speed", 1/(size/7) * 30);
   triangle.setAttribute("data-hex", '#'+(Math.random()*0xFFFFFF<<0).toString(16));
   triangle.style.animationDuration = 5 * 1/(size/7)  + "s";
   triangle.style.opacity = 0.1 + 0.8 * Math.random();
   triangle.className = "triangle";
-  return triangle;
+  var triParent = document.createElement("div");
+  triParent.setAttribute("style","position:absolute; top:" + posY + "px; left:" + posX  + "px; animation-name:move;  animation-iteration-count: 1;    animation-timing-function: linear;")
+      // triParent.style.transitionDuration = "5s";
+    // triParent.style.transform = "translateY(" + -posY + "px)";
+  triParent.style.animationDuration=  5 * 1/(size/7) + "s";
+  triParent.style.width="1px";
+  triParent.style.height="100%";
+    triParent.appendChild(triangle);
+  triParent.addEventListener("animationend", function() {
+    this.style.opacity = 0;
+    this.parentElement.removeChild(this);
+    document.getElementById("container").appendChild(createTriangle(canvas.width, canvas.height, true));
+  });
+    // triangle.style.transform += "translate(0," + -posY + "px)";
+  return triParent;
+}
+function end() {
+  this.style.borderColor = "red";
 }
 window.onload = function init() {
   document.getElementById("audio_file").onchange = function() {
@@ -42,8 +59,9 @@ window.onload = function init() {
   for (var x = 0; x < 50; x++) {
     document.getElementById("container").appendChild(createTriangle(canvas.width, canvas.height));
   }
-  triangles = document.getElementsByClassName("triangle")
+  triangles = document.getElementsByClassName("triangle");
   ctx.translate(canvas.width/2, canvas.height/2);
+
   frameLooper();
       // ctx.translate(canvas.width/2, canvas.height/2);
 }
@@ -54,17 +72,14 @@ function frameLooper() {
   ctx.clearRect(-canvas.width/2,-canvas.height/2,canvas.width, canvas.height);
   bars = 200;
   for (var x = 0; x < triangles.length; x++) {
-    triangles[x].style.top = triangles[x].getBoundingClientRect().top - 10 * triangles[x].getAttribute("data-speed") + "px";
-    if (triangles[x].offsetTop < 50) {
-      triangles[x].style.opacity = "0";
-      if (window.getComputedStyle(triangles[x]).getPropertyValue("opacity") == 0) {
-        triangles[x].parentElement.removeChild(triangles[x]);
-        document.getElementById("container").appendChild(createTriangle(canvas.width, canvas.height, true));
-      }
+      triangles[x].addEventListener("transitionend", function() {
+        console.log("end");
+      });
+        // document.getElementById("container").appendChild(createTriangle(canvas.width, canvas.height, true));
     }
 
     // triangles[x].setAttribute("rotation", (triangles[x].getAttribute("rotation") + 1) % 360);
-  }
+  // }
   for (var i = 0; i < bars; i++) {
     bar_width = 4;
     // bar_width = canvas.width/bars;
