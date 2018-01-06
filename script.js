faf39687124bc409df205dc25965588945e98b7a
aphsai <saksham.a@hotmail.ca>
2018-01-05 22:49:12 -0500

    responsive nav bar

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 window.onload = function setup() {
-  
+
+}
+function response() {
+  var x = document.getElementById("nav");
+  if (x.className === "navbar") {
+      x.className += " responsive";
+  } else {
+      x.className = "navbar";
+  }
 }
