03818fd3ec4401b40e5a209db1831b36e28410bd
aphsai <saksham.a@hotmail.ca>
2018-01-05 23:19:05 -0500

    more bug testing

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 window.onload = function setup() {
 
 }
-function response() {
-  var x = document.getElementById("nav");
-  if (x.className === "navbar") {
-      x.className += " responsive";
-  } else {
-      x.className = "navbar";
-  }
-}
