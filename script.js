410f21a3fec524510900a2149001b1c49b2f4c52
aphsai <saksham.a@hotmail.ca>
2018-03-06 16:11:40 -0500

    updated projects, fixed height issue

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 var overlay =   document.getElementById('loadingOverlay');
-  console.log("width: " + overlay.clientWidth);
   overlay.style.height = overlay.clientHeight + "px";
 window.onload = function() {
   overlay.innerHTML = '';
   var decrease = setInterval(function() {
-    console.log(overlay.style.height);
     overlay.style.height = (parseInt(overlay.style.height) - 10) + "px";
     if (parseInt(overlay.style.height) <= 10) {
       clearInterval(decrease);
