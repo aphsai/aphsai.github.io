8d32a56f0c68f6bc53cfc875aa489ee8099165fd
Saksham <saksham.a@hotmail.ca>
2020-12-25 23:31:44 -0500

    feat(website): initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div id="main">
+        <div id="main-top">
+          <h1 className="title-font title-name name-top"> SAKSHAM AGGARWAL </h1>
+        </div>
+        <div id="main-bottom">
+          <h1 className="title-font title-name name-bottom"> SAKSHAM AGGARWAL </h1>
+        </div>
+      </div>
+  );
+}
+
+export default App;
